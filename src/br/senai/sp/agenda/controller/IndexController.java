9f8b68816f617fea0ae652c6831fd83d77cee383
João Pedro <joaopedrogomesdasilva.1764@gmail.com>
2021-11-12 15:19:33 -0300

    all

---
+++ src/br/senai/sp/agenda/controller/IndexController.java
@@ -0,0 +1,28 @@
+package br.senai.sp.agenda.controller;
+
+import javafx.scene.control.Button;
+
+public class IndexController {
+	
+	private Button btnexcluirclick;
+	private Button btnadiarclick;
+	private Button btnclearclick;
+	private Button btnsalvarclick;
+	
+	public void btnclear() {
+		
+	}
+	
+public void btnexcluir() {
+		
+	}
+
+public void btnsalvar() {
+	
+}
+
+public void btnadiar() {
+	
+}	
+	
+}
---
+++ src/br/senai/sp/agenda/model/StatusTarefa.java
@@ -0,0 +1,6 @@
+package br.senai.sp.agenda.model;
+
+public enum StatusTarefa {
+	
+	ABERTA, CONCLUIDA, ADIADA
+}
